7bd59d1664257f76e128da0d3507381e4e9184dc
leondasferras <90497102+leondasferras@users.noreply.github.com>
2022-05-04 16:04:21 +0500

    add api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-// import React from 'react';
-// import { AppHeader } from './components/AppHeader/AppHeader.jsx'
-// import './App.css';
-// import { BurgerIngredients } from './components/BurgerIngredients/BurgerIngredients.jsx';
-// import { BurgerConstructor } from './components/BurgerConstructor/BurgerConstructor.jsx';
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import { AppHeader } from "./components/AppHeader/AppHeader.jsx";
+import { BurgerIngredients } from "./components/BurgerIngredients/BurgerIngredients.jsx";
+import { BurgerConstructor } from "./components/BurgerConstructor/BurgerConstructor.jsx";
 
-// const  App = () => {
-//   return (
-//     <section className="App">
-// <AppHeader/>
-// <BurgerIngredients/>
-// <BurgerConstructor/>
 
-//     </section>
-//   );
 
-// }
-// export default App;
+
+
+function App() {
+  const [state, setState] = useState({data:[]});
+  useEffect(() => {
+    const apiUrl = "https://norma.nomoreparties.space/api/ingredients";
+    const getIngredientsData = () => {
+      fetch(apiUrl)
+      .then((res) => {return res.json();
+      } )
+      .then ((res) => setState({...state, data: res.data}))
+         
+    }
+    getIngredientsData()
+  }, []);
+
+  const ingredients = state.data;
+  console.log(ingredients);
+
+  return (
+    <div className={styles.App}>
+      <AppHeader />
+      <main className={styles.main}>
+        <BurgerIngredients data={state.data} />
+        <BurgerConstructor data={state.data} />
+      </main>
+    </div>
+  );
+}
+
+export default App;
